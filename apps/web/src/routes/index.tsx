7c6553f8f0eaccd98d61cfe8fbe3fe769715f4fc
Muhammad Fadhil <technowizard17@gmail.com>
2026-04-12 06:10:16 +0700

    feat: add search params for better router integration, add notifications in root route

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,7 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { z } from 'zod';
 
 import { TasksPage } from '@/pages/tasks';
 
+const filterSchema = z.object({
+  filter: z.enum(['all', 'active', 'done']).catch('all')
+});
+
 export const Route = createFileRoute('/')({
+  validateSearch: filterSchema,
   component: TasksPage
 });
